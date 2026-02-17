49bdbbd21cd31c0829fa63415c437f265e4b50da
Toomas Kaarel <toomas.kaarel@gmail.com>
2026-02-18 00:51:57 +0200

    home categories hardcoded

--- app/home/(tabs)/_layout.tsx
+++ app/home/(tabs)/_layout.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
@@ -12,13 +11,14 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: '#4f63ac',
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarStyle: {
           paddingBottom: 20,
           borderTopWidth: 0,
           shadowColor: '#000000',
+          backgroundColor: '#fff',
           shadowOffset: {
             width: 0,               
             height: -4,             
--- app/home/(tabs)/index.tsx
+++ app/home/(tabs)/index.tsx
@@ -1,8 +1,89 @@
-import { ThemedText } from '@/components/themed-text';
-import { View } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Stack } from 'expo-router';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
-    <View>
-        <ThemedText>Homescreen</ThemedText>
-    </View>
+    return (
+        <>
+        <Stack.Screen options={{ headerTitle: 'Sign In' }} />
+        <View style={{ flex: 1, backgroundColor: '#ffffff' }}>
+            <ScrollView 
+                horizontal 
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={styles.scrollContent}
+            >
+                <View style={styles.categories}>
+                    <View style={styles.categoryContainer}>
+                        <View style={[styles.category, { backgroundColor: '#000' }] }>
+                            <FontAwesome name="star" size={30} color="white" />
+                        </View>
+                        <Text style={{ color: '#000', fontSize: 16 }}>Popular</Text>
+                    </View>
+                    <View style={styles.categoryContainer}>
+                        <View style={styles.category}>
+                            <FontAwesome5 name="chair" size={30} color="#999" />
+                        </View>
+                        <Text style={{ color: '#999', fontSize: 16 }}>Chair</Text>
+                    </View>
+                    <View style={styles.categoryContainer}>
+                        <View style={styles.category}>
+                            <MaterialIcons name="table-restaurant" size={30} color="#999" />
+                        </View>
+                        <Text style={{ color: '#999', fontSize: 16 }}>Table</Text>
+                    </View>
+                    <View style={styles.categoryContainer}>
+                        <View style={styles.category}>
+                            <MaterialCommunityIcons name="sofa-outline" size={30} color="#999" />
+                        </View>
+                        <Text style={{ color: '#999', fontSize: 16 }}>Armchair</Text>
+                    </View>
+                    <View style={styles.categoryContainer}>
+                        <View style={styles.category}>
+                            <Ionicons name="bed-outline" size={30} color="#999" />
+                        </View>
+                        <Text style={{ color: '#999', fontSize: 16 }}>Bed</Text>
+                    </View>
+                    <View style={styles.categoryContainer}>
+                        <View style={styles.category}>
+                            <MaterialCommunityIcons name="lamp-outline" size={30} color="#999" />
+                        </View>
+                        <Text style={{ color: '#999', fontSize: 16 }}>Lamp</Text>
+                    </View>
+                </View>
+            </ScrollView>
+        </View>
+        </>
+    )
 }
+
+const styles = StyleSheet.create({
+  categories: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    width: '80%',
+    flexDirection: 'row',
+  },
+  categoryContainer: {
+    flex: 1,
+    alignItems: 'center',
+    maxWidth: 60,
+    margin: '12%',
+  },
+  scrollContent: {
+    paddingHorizontal: 15, 
+    flexDirection: 'row',
+  },
+  category: {
+    height: 50,
+    width: 50,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12,
+  },
+});
