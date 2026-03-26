8c76b16562808291d0d43ca1cf8349e52192bcf7
Toomas Kaarel <toomas.kaarel@gmail.com>
2026-03-26 14:47:53 +0200

    Product list disain

--- components/product-list-item.tsx
+++ components/product-list-item.tsx
@@ -1,4 +1,5 @@
 import { IMAGES } from '@/components/ImageRegistry';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import React from 'react';
 import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
 
@@ -25,13 +26,18 @@ export function ProductListItem({
             style={styles.productContainer}
         >
             <View style={styles.product}>
-                <Image
-                    source={imageSource}
-                    style={styles.productImage}
-                    resizeMode="cover"
-                />
-                <Text style={styles.productName}>{name}</Text>
-                <Text style={styles.productPrice}>$ {price.slice(0, 2)}.{price.slice(2, 4)}</Text>
+                <AntDesign style={styles.deleteButton} name="close-circle" size={28} color="#4f63ac" />
+                <View style={styles.productImageContainer}>
+                    <Image
+                        source={imageSource}
+                        style={styles.productImage}
+                        resizeMode="cover"
+                    />
+                </View>
+                <View style={styles.productDetails}>
+                    <Text style={styles.productName}>{name}</Text>
+                    <Text style={styles.productPrice}>$ {price.slice(0, 2)}.{price.slice(2, 4)}</Text>
+                </View>
             </View>
         </Pressable>
     )
@@ -40,22 +46,32 @@ export function ProductListItem({
 const styles = StyleSheet.create({
     productContainer: {
         flex: 1, 
+        flexDirection: 'row',
         alignItems: 'center',
-        maxWidth: '50%',
-        height: 300, 
-        marginVertical: 10,  
+        justifyContent: 'space-between',
+        alignSelf: 'center',
+        width: '90%',
+        height: 180, 
+        marginVertical: 10, 
+        borderBlockColor: '#f0f0f0',
+        borderBottomWidth: 1,
     },
     product: {
         flex: 1,
-        flexDirection: 'column',
+        flexDirection: 'row',
         justifyContent: 'space-between',
-        width: '85%',
-        height: '100%', 
+        height: 150, 
     },
     productImage: {
-        width: '100%',
-        height: '75%', 
+        height: 150, 
+        width: 150, 
         borderRadius: 12,
+
+    },
+    productImageContainer: {
+        flex: 1,
+        height: 150,
+        maxWidth: 150, 
     },
     productName: {
         color: '#606060',
@@ -67,5 +83,15 @@ const styles = StyleSheet.create({
         color: '#000',
         fontFamily: 'NunitoSans-Bold',
         fontSize: 16,
+    },
+    productDetails: {
+        flex: 1,
+        paddingLeft: '8%',
+    },
+    deleteButton: {
+        position: 'absolute',
+        zIndex: 999,
+        right: 5,
+        top: 0,
     }
 })
